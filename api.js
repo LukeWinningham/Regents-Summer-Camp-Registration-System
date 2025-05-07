e9119af12d2183702d928558f2353cfa85ddd083
alyssavitug <149891949+alyssavitug@users.noreply.github.com>
2025-05-07 15:36:03 -0400

    Add try/catch block

--- api.js
+++ api.js
@@ -4,8 +4,13 @@ import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api'
 
 export const registerStudent = async (student, guardianId) => {
-  const response = await axios.post(`${API_URL}/register`, {
+  try {
+    const response = await axios.post(`${API_URL}/register`, {
     student, guardianId
   })
   return response.data
+} catch (error) {
+  console.error('Registration Failed', error)
+  throw error
+  }
 }
